2056a946e0b15cf4dfa94c781ee995159176258d
kiancn <kian.tema@gmail.com>
2020-05-18 14:04:09 +0200

    Former HomeController.java is now DemoController.java. A new home controller will emerge soon.

---
+++ src/main/java/kea/motorhome/motorhomesite/util/DBConnectionManager.java
@@ -0,0 +1,57 @@
+package kea.motorhome.motorhomesite.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+
+public class DBConnectionManager
+{
+    private static String user;
+    private static String password;
+    private static String url;
+
+    private static Connection connection;
+
+    private DBConnectionManager(){}
+
+    public static Connection getConnection()
+    {
+        if(connection != null){return connection;}
+        return connection = getDatabaseConnection();
+    }
+
+    private static Connection getDatabaseConnection() {
+        Properties prop = new Properties();
+        try {
+            FileInputStream propertyFile = new FileInputStream("src/main/resources/application.properties");
+            prop.load(propertyFile);
+            user = prop.getProperty("db.user");
+            password = prop.getProperty("db.password");
+            url = prop.getProperty("db.url");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File could not be found");
+            e.printStackTrace();
+        }
+        catch(IOException e){
+            System.out.println("Property could not be loaded");
+            e.printStackTrace();
+        }
+        try
+        {
+            return DriverManager.getConnection(url, user, password);
+        } catch(SQLException e)
+        {
+            System.out.println("SQL Exception while establishing connection.");
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
