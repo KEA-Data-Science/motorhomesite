23a112bc0a6fc27fc1db0a009a19528a276b7b77
Thv88 <jublen909@gmail.com>
2020-05-20 12:56:44 +0200

    Included carmodel as an attribute to motorhome

--- src/main/java/kea/motorhome/motorhomesite/controllers/DemoController.java
+++ src/main/java/kea/motorhome/motorhomesite/controllers/DemoController.java
@@ -1,6 +1,9 @@
 package kea.motorhome.motorhomesite.controllers;
 
+import kea.motorhome.motorhomesite.daodemo.MotorhomeDAODemo;
+import kea.motorhome.motorhomesite.models.CarModel;
 import kea.motorhome.motorhomesite.models.MobileHomeModel;
+import kea.motorhome.motorhomesite.models.Motorhome;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,35 +14,39 @@ import java.util.List;
 @Controller
 public class DemoController
 {
-    List<MobileHomeModel> mobileHomes;
+    List<Motorhome> mobileHomes;
 
     @GetMapping("/")
     public String home(){ return "index"; }
 
 
     @GetMapping("/gallery")
-    public String galleryPage(Model model)
-    {
+    public String galleryPage(Model model) {
+        MotorhomeDAODemo motorhomeDAODemo = new MotorhomeDAODemo();
+
         mobileHomes = new ArrayList<>();
-        mobileHomes.add(new MobileHomeModel(1,"Big One Runnrunn",2018
+        mobileHomes.add(new Motorhome(1,new CarModel("Big One Runnrunn", "UHYTG4334", 120, 3),2018
                 ,"This wonderful vehicle can take you anywhere as long as it is not uphill.",
                                             203.0f,4,"/img/MH1.jpg"));
-        mobileHomes.add(new MobileHomeModel(1,"Busty Deep Seater",2014
+        mobileHomes.add(new Motorhome(1, new CarModel("Busty Deep Seater", "UHYTG4334", 150, 2),2014
                 ,"Thompsons Teeth are the Only Teeth that can Chew Other Teeth! Also, this is not a vehicle" +
                  ", but a prop useful only for pseudo-pornographic and semi-educational purposes.",
                                             203.0f,4,"../img/MH2.jpg"));
-        mobileHomes.add(new MobileHomeModel(1,"Normal Mobile Home",2004
+        mobileHomes.add(new Motorhome(1, new CarModel("Normal Mobile Home", "UHYTG4334", 170, 2), 2004
                 ,"If you rent this car, nothing in your life will be the same. Literally, you will be " +
                  "transported to another Universe without oxygen, and sadly, suffocate. The good news is, " +
                  "you won't really be real at that point, because the laws of physics in that Universe do " +
                  "not allow matter to subsist.",
                                             203.0f,4,"img/MH3.jpg"));
-        mobileHomes.add(new MobileHomeModel(1,"Heritage Mobile Home",1954
+        mobileHomes.add(new Motorhome(1, new CarModel("Heritage Mobile Home", "UHYTG4334", 170, 2), 1954
                 ,"Winston Churchill spent his last four summers traveling around this old baby. She " +
                  "traveled the Alps before Tiger Woods. Anyway, the vehicle is a complete deathtrap.",
                                             203.0f,4,"../img/MH4.jpg"));
 
-        model.addAttribute("mobileHomes",mobileHomes);
+
+
+        model.addAttribute("mobileHomes", mobileHomes);
         return "gallery";
     }
+
 }
--- src/main/java/kea/motorhome/motorhomesite/models/CarModel.java
+++ src/main/java/kea/motorhome/motorhomesite/models/CarModel.java
@@ -2,4 +2,48 @@ package kea.motorhome.motorhomesite.models;
 
 public class CarModel
 {
+    String modelName;
+    String modelnumber;
+    int horsePower;
+    int beds;
+
+
+    public CarModel(String modelName, String modelnumber, int horsePower, int beds) {
+        this.modelName = modelName;
+        this.modelnumber = modelnumber;
+        this.horsePower = horsePower;
+        this.beds = beds;
+    }
+
+    public String getModelnumber() {
+        return modelnumber;
+    }
+
+    public void setModelnumber(String modelnumber) {
+        this.modelnumber = modelnumber;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public void setBeds(int beds) {
+        this.beds = beds;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
 }
