1f68584e323a05b796399e16411545c13dcf7a79
kiancn <kian.tema@gmail.com>
2020-05-28 22:29:40 +0200

    Merge branch 'master' of W:\GitHub\motorhomesite with conflicts.

--- src/main/java/kea/motorhome/motorhomesite/dao/CarModelDAO.java
+++ src/main/java/kea/motorhome/motorhomesite/dao/CarModelDAO.java
@@ -2,6 +2,7 @@ package kea.motorhome.motorhomesite.dao;
 
 import kea.motorhome.motorhomesite.models.CarModel;
 import kea.motorhome.motorhomesite.util.DBConnectionManager;
+import org.springframework.objenesis.SpringObjenesis;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -10,8 +11,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CarModelDAO implements IDAO<CarModel, Integer> {
-
+public class CarModelDAO implements IDAO<CarModel, Integer>
+{
     private Connection connection;
     private SiteDAOCollection dao;
 
@@ -21,157 +22,196 @@ public class CarModelDAO implements IDAO<CarModel, Integer> {
         dao = SiteDAOCollection.getInstance();
     }
 
+    /**
+     * Creates a new entity in data-source based on the supplied thing.
+     * Returns true if successful.
+     *
+     * @param thing
+     */
     @Override
     public boolean create(CarModel thing)
     {
         try
         {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "INSERT INTO motorhome.carModel (modelName, horsePower, beds, engineCapacity, length, height, width, weight, hotWaterCapacity, coldWaterCapacity, numberOfSeats, oven, cruiseControl, shower) " +
-                            "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+                    "INSERT INTO motorhome.carmodel (" +
+                    "modelName, " +
+                    "modelnumber, " +
+                    "horsePower, " +
+                    "beds, " +
+                    "engineCapacity, " +
+                    "length," +
+                    "width," +
+                    "height," +
+                    "weight," +
+                    "hotWaterCapacity," +
+                    "coldWaterCapacity," +
+                    "numberOfSeats," +
+                    "oven," +
+                    "cruiseControl," +
+                    "shower) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+
             preparedStatement.setString(1, thing.getModelName());
-            preparedStatement.setInt(2, thing.getHorsePower());
-            preparedStatement.setInt(3, thing.getBeds());
-            preparedStatement.setFloat(4, thing.getEngineCapacity());
-            preparedStatement.setFloat(5, thing.getLength());
-            preparedStatement.setFloat(6, thing.getHeight());
+            preparedStatement.setString(2, thing.getModelnumber());
+            preparedStatement.setInt(3, thing.getHorsePower());
+            preparedStatement.setInt(4, thing.getBeds());
+            preparedStatement.setFloat(5, thing.getEngineCapacity());
+            preparedStatement.setFloat(6, thing.getLength());
             preparedStatement.setFloat(7, thing.getWidth());
-            preparedStatement.setFloat(8, thing.getWeight());
-            preparedStatement.setFloat(9, thing.getHotWaterCapacity());
-            preparedStatement.setFloat(10, thing.getColdWaterCapacity());
-            preparedStatement.setInt(11, thing.getNumberOfSeats());
-            preparedStatement.setBoolean(12, thing.isOven());
-            preparedStatement.setBoolean(13, thing.isCruiseControl());
-            preparedStatement.setBoolean(14, thing.isShower());
+            preparedStatement.setFloat(8, thing.getHeight());
+            preparedStatement.setFloat(9, thing.getWeight());
+            preparedStatement.setFloat(10, thing.getHotWaterCapacity());
+            preparedStatement.setFloat(11, thing.getColdWaterCapacity());
+            preparedStatement.setInt(12, thing.getNumberOfSeats());
+            preparedStatement.setByte(13, (byte)(!thing.isOven() ? 0 : 1)); // converting bool to byte
+            preparedStatement.setByte(14, (byte)(!thing.isCruiseControl() ? 0 : 1));
+            preparedStatement.setByte(15, (byte)(!thing.isShower() ? 0 : 1));
 
             return preparedStatement.executeUpdate() > 0;
-
-        } catch(SQLException e) { e.printStackTrace(); }
-
+        } catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
         return false;
     }
 
-    private void fillCarModelValuesFromResultSet(CarModel carModel, ResultSet resultSet) throws SQLException
-    {
-        carModel.setCarModelID(resultSet.getInt(1));
-        carModel.setModelName(resultSet.getString(2));
-        carModel.setHorsePower(resultSet.getInt(3));
-        carModel.setBeds(resultSet.getInt(4));
-        carModel.setEngineCapacity(resultSet.getFloat(5));
-        carModel.setLength(resultSet.getFloat(6));
-        carModel.setHeight(resultSet.getFloat(7));
-        carModel.setWidth(resultSet.getFloat(8));
-        carModel.setWeight(resultSet.getFloat(9));
-        carModel.setHotWaterCapacity(resultSet.getFloat(10));
-        carModel.setColdWaterCapacity(resultSet.getFloat(11));
-        carModel.setNumberOfSeats(resultSet.getInt(12));
-        carModel.setOven(resultSet.getBoolean(13));
-        carModel.setCruiseControl(resultSet.getBoolean(14));
-        carModel.setShower(resultSet.getBoolean(15));
-    }
-
+    /**
+     * Returns a T-type object, read from data-source
+     * - queried with supplied id (type U).
+     * Returns null if there is no 'thing' by id in db.
+     *
+     * @param id
+     */
     @Override
     public CarModel read(Integer id)
     {
-        CarModel carModel = new CarModel();
+        CarModel model = new CarModel();
         try
         {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM motorhome.carModel WHERE idcarModel = ?");
+                    "SELECT * FROM motorhome.carmodel WHERE idcarModel = ?");
             preparedStatement.setInt(1, id);
+
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while(resultSet.next()) { fillCarModelValuesFromResultSet(carModel, resultSet); }
+            while(resultSet.next()) {fillCarModelValuesFromResultSet(model, resultSet);}
 
-            return carModel;
+            return model;
 
         } catch(SQLException e) { e.printStackTrace(); }
 
         return null;
     }
 
+    private void fillCarModelValuesFromResultSet(CarModel model, ResultSet resultSet) throws SQLException
+    {
+        model.setCarModelID(resultSet.getInt(1));
+        model.setModelName(resultSet.getString(2));
+        model.setModelnumber(resultSet.getString(3));
+        model.setHorsePower(resultSet.getInt(4));
+        model.setBeds(resultSet.getInt(5));
+        model.setEngineCapacity(resultSet.getFloat(6));
+        model.setLength(resultSet.getFloat(7));
+        model.setWidth(resultSet.getFloat(8));
+        model.setHeight(resultSet.getFloat(9));
+        model.setWeight(resultSet.getFloat(10));
+        model.setHotWaterCapacity(resultSet.getFloat(11));
+        model.setColdWaterCapacity(resultSet.getFloat(12));
+        model.setNumberOfSeats(resultSet.getInt(13));
+        model.setOven(resultSet.getByte(14) != 0);
+        model.setCruiseControl(resultSet.getByte(15) != 0);
+        model.setShower(resultSet.getByte(16) != 0);
+    }
+
+    /**
+     * Method queries data-source and returns complete list of paralleled
+     * object of type T wrapped in a List
+     */
     @Override
     public List<CarModel> readall()
     {
         List<CarModel> carModels = new ArrayList<>();
+
         try
         {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "SELECT * FROM motorhome.carModel");
+                    "SELECT * FROM motorhome.carmodel");
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while(resultSet.next())
             {
-                CarModel carModel = new CarModel();
-                fillCarModelValuesFromResultSet(carModel, resultSet);
-                carModels.add(carModel);
+                CarModel model = new CarModel();
+                fillCarModelValuesFromResultSet(model, resultSet);
+                carModels.add(model);
             }
-        } catch(SQLException e) { e.printStackTrace(); }
+        } catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
 
         return carModels;
     }
 
+    /**
+     * Method executes update to DB based on supplied thing type-T:
+     * Returns true if update was written to DB, false if nothing was written.
+     *
+     * @param thing
+     */
     @Override
     public boolean update(CarModel thing)
     {
         try
         {
             PreparedStatement preparedStatement = connection.prepareStatement(
-                    "UPDATE motorhome.carModel SET " +
-                            "modelName = ?," +
-                            "horsePower = ?," +
-                            "beds = ?," +
-                            "enginecapacity = ?" +
-                            "length = ?" +
-                            "height = ?" +
-                            "width = ?" +
-                            "weight = ?" +
-                            "hotWaterCapacity = ?" +
-                            "coldWaterCapacity = ?" +
-                            "numberOfSeats = ?" +
-                            "oven = ?" +
-                            "cruiseControl = ?" +
-                            "shower = ?" +
-                            "WHERE idcarModel = ?");
+                    "UPDATE motorhome.carmodel SET " +
+                    "modelName = ?," +
+                    "modelnumber = ?," +
+                    "horsePower = ?," +
+                    "beds = ?," +
+                    "engineCapacity = ?," +
+                    "length = ?," +
+                    "width = ?," +
+                    "height = ?," +
+                    "weight = ?," +
+                    "hotWaterCapacity = ?," +
+                    "coldWaterCapacity = ?," +
+                    "numberOfSeats = ?," +
+                    "oven = ?," +
+                    "cruiseControl = ?," +
+                    "shower = ? WHERE idcarModel = ?");
 
-            preparedStatement.setString(1, thing.getModelName());
-            preparedStatement.setInt(2, thing.getHorsePower());
-            preparedStatement.setInt(3, thing.getBeds());
-            preparedStatement.setFloat(4, thing.getEngineCapacity());
-            preparedStatement.setFloat(5, thing.getLength());
-            preparedStatement.setFloat(6, thing.getHeight());
-            preparedStatement.setFloat(7, thing.getWidth());
-            preparedStatement.setFloat(8, thing.getWeight());
-            preparedStatement.setFloat(9, thing.getHotWaterCapacity());
-            preparedStatement.setFloat(10, thing.getColdWaterCapacity());
-            preparedStatement.setInt(11, thing.getNumberOfSeats());
-            preparedStatement.setBoolean(12, thing.isOven());
-            preparedStatement.setBoolean(13, thing.isCruiseControl());
-            preparedStatement.setBoolean(14, thing.isShower());
+            preparedStatement.setString(1,thing.getModelName());
+            preparedStatement.setString(2,thing.getModelnumber());
+            preparedStatement.setInt(3,thing.getHorsePower());
+            preparedStatement.setInt(4, thing.getBeds());
+            preparedStatement.setFloat(5,thing.getEngineCapacity());
+            preparedStatement.setFloat(5,thing.getLength());
+            preparedStatement.setFloat(6,thing.getWidth());
+            preparedStatement.setFloat(7,thing.getHeight());
+            preparedStatement.setFloat(8,thing.getWeight());
+            preparedStatement.setFloat(8,thing.getHotWaterCapacity());
+            preparedStatement.setFloat(8,thing.getColdWaterCapacity());
+            preparedStatement.setInt(9,thing.getNumberOfSeats());
+            preparedStatement.setByte(10, (byte)(thing.isOven()?1:0));
+            preparedStatement.setByte(11, (byte)(thing.isCruiseControl()?1:0));
+            preparedStatement.setByte(12, (byte)(thing.isShower()?1:0));
 
-            return preparedStatement.executeUpdate() > 0;
-
-        } catch(SQLException e) { e.printStackTrace(); }
+        } catch()
 
         return false;
     }
 
+    /**
+     * Method removes object from data-source, where data-source entity-id
+     * equals the supplied id, and returns a thing of type U
+     *
+     * @param id
+     */
     @Override
     public boolean delete(Integer id)
     {
-        try
-        {
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                    "DELETE FROM motorhome.carModel WHERE idcarModel = ?");
-            preparedStatement.setInt(1, id);
-
-            return preparedStatement.executeUpdate() > 0;
-
-        } catch(SQLException e) { e.printStackTrace(); }
-
         return false;
     }
 }
-
-
--- src/main/java/kea/motorhome/motorhomesite/dao/ServiceDAO.java
+++ src/main/java/kea/motorhome/motorhomesite/dao/ServiceDAO.java
@@ -1,53 +1,153 @@
 package kea.motorhome.motorhomesite.dao;
 
 import kea.motorhome.motorhomesite.models.Service;
+import kea.motorhome.motorhomesite.util.DBConnectionManager;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ServiceDAO implements IDAO<Service,Integer> {
+public class ServiceDAO implements IDAO<Service, Integer>
+{
 
-	private Connection connection;
+    private Connection connection;
+    private SiteDAOCollection dao;
 
-	/**
-	 * @param thing
-	 */
-	@Override
-	public boolean create(Service thing)
-	{
-		return false;
-	}
+    public ServiceDAO()
+    {
+        connection = DBConnectionManager.getConnection();
+        dao = SiteDAOCollection.getInstance();
+    }
 
-	/**
-	 * @param id
-	 */
-	@Override
-	public Service read(Integer id)
-	{
-		return null;
-	}
+    /**
+     * @param thing
+     */
+    @Override
+    public boolean create(Service thing)
+    {
+        try
+        {        /* construction notes; error in DB-model, will not work before db-fix 28/5 */
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO motorhome.service (name, description, unitPrice) " +
+                    "VALUES (?,?,?)");
 
-	@Override
-	public List<Service> readall()
-	{
-		return null;
-	}
+            preparedStatement.setString(1, thing.getName());
+            preparedStatement.setString(1, thing.getName());
+            preparedStatement.setFloat(1, thing.getUnitPrice());
 
-	/**
-	 * @param thing
-	 */
-	@Override
-	public boolean update(Service thing)
-	{
-		return false;
-	}
+            return preparedStatement.executeUpdate() > 0;
 
-	/**
-	 * @param id
-	 */
-	@Override
-	public boolean delete(Integer id)
-	{
-		return false;
-	}
+        } catch(SQLException e) { e.printStackTrace(); }
+
+        return false;
+    }
+
+    /**
+     * @param id
+     */
+    @Override
+    public Service read(Integer id)
+    {
+        Service service = new Service();
+
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM motorhome.service WHERE idService = ?");
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next())
+            {
+                fillServiceValuesFromResultSet(service, resultSet);
+            }
+
+            return service;
+        } catch(SQLException e) {e.printStackTrace(); }
+
+        return null;
+    }
+
+    private void fillServiceValuesFromResultSet(Service service, ResultSet resultSet) throws SQLException
+    {
+        service.setServiceID(resultSet.getInt(1));
+        service.setName(resultSet.getString(2));
+        service.setDescription(resultSet.getString(3));
+        service.setUnitPrice(resultSet.getInt(4));
+    }
+
+    @Override
+    public List<Service> readall()
+    {
+        List<Service> services = new ArrayList<>();
+
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "SELECT * FROM motorhome.service");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next())
+            {
+                Service service = new Service();
+                fillServiceValuesFromResultSet(service, resultSet);
+                services.add(service);
+            }
+            return services;
+        } catch(SQLException e) {e.printStackTrace(); }
+
+        return null;
+    }
+
+    /**
+     * @param thing
+     */
+    @Override
+    public boolean update(Service thing)
+    {
+        try
+        {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "UPDATE motorhome.service SET " +
+                    "name = ?," +
+                    "description = ?," +
+                    "unitPrice = ?" +
+                    "WHERE idService = ?");
+
+            preparedStatement.setString(1, thing.getName());
+            preparedStatement.setString(2, thing.getDescription());
+            preparedStatement.setFloat(3, thing.getUnitPrice());
+            preparedStatement.setInt(4, thing.getServiceID());
+
+            return preparedStatement.executeUpdate() > 0;
+
+        } catch(SQLException e) { e.printStackTrace(); }
+
+        return false;
+    }
+
+    /**
+     * @param id
+     */
+    @Override
+    public boolean delete(Integer id)
+    {
+    	try
+		{
+			PreparedStatement preparedStatement = connection.prepareStatement(
+					"DELETE FROM motorhome.service WHERE idService = ?");
+			preparedStatement.setInt(1, id);
+
+			return preparedStatement.executeUpdate() > 0;
+		}catch(SQLException e){
+    		e.printStackTrace();
+		}
+
+        return false;
+    }
 }
