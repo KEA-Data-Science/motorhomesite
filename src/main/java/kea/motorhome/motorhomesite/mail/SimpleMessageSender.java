38ed664a01b73be8e563ca892be568447d17b8cd
kiancn <kian.tema@gmail.com>
2020-05-30 15:52:19 +0200

    Many DAOs implemented, controllers updated; sql updated

--- src/main/java/kea/motorhome/motorhomesite/mail/SimpleMessageSender.java
+++ src/main/java/kea/motorhome/motorhomesite/mail/SimpleMessageSender.java
@@ -40,6 +40,9 @@ public class SimpleMessageSender
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.debug", "true");
 
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+
+
         return mailSender;
     }
 
